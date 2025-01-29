f439aae7167de7607e68aefd4e30cde4cd39a3c9
20149466 <asima24.barik@gmail.com>
2025-01-29 12:51:45 -0500

    APISchemaValidationTest is added

---
+++ src/test/java/com/qa/gorest/tests/APISchemaValidationTest.java
@@ -0,0 +1,24 @@
+package com.qa.gorest.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.gorest.base.BaseTest;
+import com.qa.gorest.client.RestClient;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+public class APISchemaValidationTest extends BaseTest {
+
+	@BeforeMethod
+	public void setup() {
+		restClient=new RestClient(prop,baseURI);		
+	}
+	
+	@Test
+	public void getUserAPIschemaValidationTest() {
+		restClient.get("/public/v2/users/", true,true)
+		.then().log().all()
+		.assertThat().statusCode(200)
+		.body(matchesJsonSchemaInClasspath("GetUserSchema.json"));
+		
+	}
+}
